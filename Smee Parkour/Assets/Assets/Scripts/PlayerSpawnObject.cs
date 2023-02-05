120d9049c5a245d2c51c7b1d8fc2dd8ef071cc2f
gloomy <ahren.val@gmail.com>
2023-02-05 17:35:36 +1100

    Changes 0.4.1
    - Changed spawning
    - Added despawning

--- Smee Parkour/Assets/Assets/Scripts/PlayerSpawnObject.cs
+++ Smee Parkour/Assets/Assets/Scripts/PlayerSpawnObject.cs
@@ -24,9 +24,9 @@ public class PlayerSpawnObject : NetworkBehaviour
             SpawnObject(enemy, transform, this); // sends method to server with the object to spawn, transform, and script that called it  
         }
 
-        if (spawnedObject == null && Input.GetKeyDown(KeyCode.Alpha1))
+        if (spawnedObject != null && Input.GetKeyDown(KeyCode.Alpha2))
         {
-
+            DespawnObject(spawnedObject);
         }
     }
 
@@ -35,7 +35,7 @@ public class PlayerSpawnObject : NetworkBehaviour
     {
         GameObject spawned = Instantiate(obj, player.position + player.forward, Quaternion.identity); // create object from the call of a client
         ServerManager.Spawn(spawned); // spawn object in the server world --> can be seen by all clients
-        //SetSpawnedObject(spawned, script); // call client function to set spawnedObject parameter to the object for EVERY client so all client know they CANT spawn another object
+        SetSpawnedObject(spawned, script); // call client function to set spawnedObject parameter to the object for EVERY client so all client know they CANT spawn another object
         // ^ acts as a toggle to prevent more spawned objects as ALL clients not know they cannot spawn another because there is an object in the scripts parameter
         // ^ due to if (spawnedObj = null) in if statement in the Update function
     }
@@ -46,4 +46,10 @@ public class PlayerSpawnObject : NetworkBehaviour
         script.spawnedObject = spawned;
     }
 
+
+    [ServerRpc(RequireOwnership = false)] // you can spawn objects with 'Ownership'
+    public void DespawnObject(GameObject obj)
+    {
+        ServerManager.Despawn(obj);
+    }
 }
